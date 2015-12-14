4d10277fd06ef39b92c4c0426c35905b2603d62e
Mischa Holz <m.holz@mvb-online.de>
2015-12-14 15:59:26 +0100

    Added visualization

--- src/main/java/de/mvbonline/jmeteranalyzer/backend/graph/GraphAnalyzer.java
+++ src/main/java/de/mvbonline/jmeteranalyzer/backend/graph/GraphAnalyzer.java
@@ -1,12 +1,17 @@
 package de.mvbonline.jmeteranalyzer.backend.graph;
 
 import de.mvbonline.jmeteranalyzer.backend.base.Analyzer;
+import net.lingala.zip4j.core.ZipFile;
+import org.apache.commons.io.IOUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.PrintWriter;
+import java.io.*;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -14,12 +19,98 @@ import java.util.List;
  */
 public class GraphAnalyzer implements Analyzer {
 
+    private static final int PAGE_SIZE = 100_000;
+
     @Override
     public void analyze(Connection sqliteConnection, List<String> tables, PrintWriter resultsFileWriter, File resultDir) throws Exception {
-//        BufferedImage image = new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
-//
-//
-//
-//        ImageIO.write(image, "png", new File(resultDir, "image.png"));
+
+        boolean written = false;
+
+        for (String table : tables) {
+            System.out.println("Creating graph files for " + table + "...");
+            long results = 0;
+
+            while(true) {
+                StringBuilder jsData = new StringBuilder();
+                String delim = "";
+
+                Date start = null;
+                Date end = null;
+
+                try(Statement statement = sqliteConnection.createStatement()) {
+                    String sql = "SELECT timestamp, duration, success FROM '" + table + "' ORDER BY timestamp ASC LIMIT " + PAGE_SIZE + " OFFSET " + results;
+                    try(ResultSet resultSet = statement.executeQuery(sql)) {
+                        boolean foundAny = false;
+                        while(resultSet.next()) {
+                            foundAny = true;
+
+
+                            long timestamp = resultSet.getLong("timestamp");
+                            if(start == null) {
+                                start = new Date(timestamp);
+                            }
+                            end = new Date(timestamp);
+
+                            jsData.append(delim);
+                            delim = ",\n";
+
+                            jsData.append("{date: ");
+                            jsData.append(timestamp).append(", \n");
+
+                            boolean success = resultSet.getBoolean("success");
+                            if(success) {
+                                jsData.append("lineColor: '#5fb503',");
+                            } else {
+                                jsData.append("lineColor: '#ff0000',");
+                            }
+
+                            jsData.append("duration: ");
+
+                            long duration = resultSet.getLong("duration");
+                            jsData.append(duration).append("}");
+
+                            results++;
+                        }
+
+                        if(!foundAny) {
+                            break;
+                        }
+                    }
+                }
+
+                System.out.println("Writing page...");
+                try(InputStream is = GraphAnalyzer.class.getResourceAsStream("/graphs/graph.html")) {
+                    String file = IOUtils.toString(is);
+                    file = file.replace("$RESULT_DATA$", jsData.toString());
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HHmmssSSS");
+                    String startString = sdf.format(start);
+                    String endString = sdf.format(end);
+
+                    File resultFile = new File(resultDir, table + "-" + startString + "-" + endString + ".duration-graph.html");
+                    try(OutputStream os = new FileOutputStream(resultFile)) {
+                        IOUtils.write(file, os);
+                    }
+                }
+
+                written = true;
+            }
+            System.out.println("Done");
+        }
+
+        if(written) {
+            System.out.println("Extracting JS libraries to result folder...");
+            File zipFile = new File(resultDir, "amcharts.zip");
+            try(InputStream is = GraphAnalyzer.class.getResourceAsStream("/graphs/amcharts.zip");
+                OutputStream os = new FileOutputStream(zipFile)) {
+                IOUtils.copyLarge(is, os);
+            }
+
+            ZipFile zip = new ZipFile(zipFile);
+            zip.extractAll(resultDir.getAbsolutePath());
+
+            boolean test = zipFile.delete();
+            System.out.println("Done");
+        }
     }
 }
